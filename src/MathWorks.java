2c66e3934d75748bf0304f29012d401ae914d9af
SrinivasanNikhil <nikhil.srinivasan@gmail.com>
2018-01-29 07:57:30 -0500

    updates

--- src/MathWorks.java
+++ src/MathWorks.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MathWorks {
@@ -5,9 +6,18 @@ public class MathWorks {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		final double PI = 3.14;
+		
+		//final is a keyword used in front of attributes whose value does not change
+		final double PI = (double) 22/7;
+		
+		final double SPEED_OF_LIGHT = 300000000; //
+		
+		System.out.println(PI);
+		System.out.println(SPEED_OF_LIGHT);
 		
 		//Math class, its a static class since Math does not change
+		//No need to make an object of the class
+		//reading the documentation of a class, called its API
 		
 		int x = 10;
 		int y = 20;
@@ -22,6 +32,8 @@ public class MathWorks {
 		
 		System.out.println(Math.min(aNumber, bNumber));
 		System.out.println(Math.pow(aNumber, bNumber));
+		
+		final double SPEED_OF_LIGHT_1 =  3 * Math.pow(10, 8);
 		
 		
 		// Java automatically converts one type into another, called implicit casting
@@ -39,7 +51,49 @@ public class MathWorks {
 		//int double float char are all primitive data types, String is an object type
 		//String data is an object
 		
+		String name = "Nikhil Srinivasan";
+		String anotherName = "Akhil";
+		
+		//since String is an object , it has an associated class
+				//String methods
+					 // .equals, .contains, .toLowerCase, 
+				
+		
+		System.out.println(name.equals("Nikhil"));
+		
+		System.out.println(name.equals(anotherName));
+		
+		
+		//boolean a type of data, holds true and false values.
+		
+		
 		//scan.next() vs scan.nextLine()
+		//the Scanner class also has several methods, some of which we have seen
+		
+		System.out.print("Please type in a word >");
+		String aWord = scan.next();
+		
+		//System.out.print("Please type in a sentence >");
+		//String aSent = scan.next();
+		
+		System.out.print("Please type in a sentence >");
+		String aSent = scan.nextLine();
+		
+		System.out.println(aWord);
+		System.out.println(aSent);
+		
+		
+		
+		Random randomGen = new Random();
+		
+		int number = randomGen.nextInt();
+		
+		
+		
+		number = randomGen.nextInt(6);
+		
+		
+		
 		
 		
 		
